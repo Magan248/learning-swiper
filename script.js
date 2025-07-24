f286b733610ef6069cdc9f65ef6cf3900888f4a9
Magan Singh <Magan.netlify@gmail.com>
2025-07-24 12:51:36 +0530

    add minify short command in json  and in js file loop:true

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 var swiper5 = new Swiper(".mySwiper5", {
-      loop:true,
-      effect: "flip",
-      grabCursor: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable:true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-        
-      },
-    });
+  loop: true,
+  effect: "flip",
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-
-    // *************************
-    var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 10,
-      slidesPerView: 6,
-      freeMode: true,
-      watchSlidesProgress: true,
-    });
-    var swiper2 = new Swiper(".mySwiper2", {
-      spaceBetween: 10,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      thumbs: {
-        swiper: swiper,
-      },
-    });
+// *************************
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
